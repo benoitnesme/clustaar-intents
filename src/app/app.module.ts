f482f43897fa80426b9a1e01fac85a73b516cf96
benoitnesme <benoit.nesme@gmail.com>
2018-08-01 08:17:22 +0200

    add material and flew layout to app.* (root)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { IntentTableComponent } from './intent/intent-table/intent-table.component';
 import { ViewIntentComponent } from './intent/view-intent/view-intent.component';
 import { IntentService } from './intent/intent/intent.service';
 import { IntentServiceFake } from './intent/intent/intent.service.fake';
+import { MaterialModule } from './/material.module';
 @NgModule({
   declarations: [AppComponent, IntentTableComponent, ViewIntentComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
   providers: [{ provide: IntentService, useClass: IntentServiceFake }],
   bootstrap: [AppComponent],
 })
